7dfb23373ba80cd4dc5d121e6dd396cd1e730d57
LMigliet <lmigliet@ic.ac.uk>
2024-07-25 20:35:03 +0100

    wrapp and log data extractor

--- src/extractor.py
+++ src/extractor.py
@@ -1,7 +1,21 @@
+"""
+extractor.py
+
+This module contains functions to extract and process 
+raw amplification data from dPCR machines.
+It includes functions to extract curves from individual 
+files and to process all files in a specified folder.
+"""
+
+import logging
 import os
 
 import pandas as pd
 from tqdm import tqdm
+
+from src.logging_config import setup_logger
+
+logger = setup_logger(__name__)
 
 
 def extract_curves(filename):
@@ -19,11 +33,16 @@ def extract_curves(filename):
     pandas.DataFrame
         DataFrame with fluorescence values and cycles as columns.
     """
-    df = pd.read_csv(filename, sep="\t", header=None)
-    df.index = df.iloc[:, 0]
-    df = df.iloc[:, 1::2]
-    df.index.name = None
-    return df
+    try:
+        df = pd.read_csv(filename, sep="\t", header=None)
+        df.index = df.iloc[:, 0]
+        df = df.iloc[:, 1::2]
+        df.index.name = None
+        logger.info(f"Successfully extracted data from {filename}")
+        return df
+    except Exception as e:
+        logger.error(f"Error processing file {filename}: {e}")
+        return pd.DataFrame()
 
 
 def process_files_in_folder(folder_path):
@@ -50,18 +69,25 @@ def process_files_in_folder(folder_path):
         if os.path.isfile(os.path.join(folder_path, file))
     ]
 
-    for ac_file in tqdm(ac_files, desc="Reading AC Files", leave=False):
+    for ac_file in ac_files:
         panel_id = int(
             os.path.basename(ac_file)[5:7]
         )  # Extract panel id from the filename and convert to integer
 
         # Extract and process the data from the file
         single_df = extract_curves(ac_file).T
+        if single_df.empty:
+            logger.warning(f"No data extracted from {ac_file}, skipping.")
+            continue
         single_df = single_df.astype(float)
         single_df["Panel"] = panel_id
         single_df = single_df.reset_index()
 
         combined_df_list.append(single_df)
+
+    if not combined_df_list:
+        logger.warning("No valid data files found.")
+        return pd.DataFrame()
 
     combined_df = pd.concat(combined_df_list)
 
@@ -75,5 +101,38 @@ def process_files_in_folder(folder_path):
         combined_df = combined_df.drop(columns=["index"])
 
     combined_df = combined_df.reset_index(drop=True)
-    print("Raw Data Extracted from TXT files")
+    logger.info("Raw data extracted from TXT files")
     return combined_df
+
+
+def load_data(folder_path, metadata_path, log_level=logging.INFO):
+    """
+    Load the raw data and metadata, merge them into a single DataFrame.
+
+    Parameters
+    ----------
+    folder_path : str
+        Path to the folder containing raw data files.
+    metadata_path : str
+        Path to the metadata CSV file.
+    log_level : int
+        The logging level.
+
+    Returns
+    -------
+    pd.DataFrame
+        Merged DataFrame containing raw data and metadata.
+    """
+    logger.setLevel(log_level)
+
+    df_ac = process_files_in_folder(folder_path)
+    if df_ac.empty:
+        raise ValueError("No data extracted from the provided folder path.")
+    df_meta = pd.read_csv(metadata_path)
+
+    df_raw = df_meta.merge(df_ac, on="Panel").reset_index(drop=True)
+    logger.info(
+        f"Meta shape: {df_meta.shape} | Raw data shape: {df_ac.shape} | Final DF shape: {df_raw.shape}\n"
+    )
+
+    return df_raw
